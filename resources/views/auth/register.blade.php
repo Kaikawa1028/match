36ec807d52ea17edf4f7142f845f12b1846bd455
KAikawa10 <stbh1028@gmail.com>
2020-05-13 11:23:27 +0900

    登録画面に性別を追加

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -61,6 +61,23 @@
                             </div>
                         </div>
 
+                        <div class="form-group row">
+                            <label for="sex" class="col-md-4 col-form-label text-md-right">{{ __('sex') }}</label>
+
+                            <div class="col-md-2">
+                                <select id="sex" name="sex" class="form-control">
+                                    <option value="1">男性</option>
+                                    <option value="0">女性</option>
+                                </select>
+
+                                @error('sex')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+                            </div>
+                        </div>
+
                         <div class="form-group row mb-0">
                             <div class="col-md-6 offset-md-4">
                                 <button type="submit" class="btn btn-primary">
