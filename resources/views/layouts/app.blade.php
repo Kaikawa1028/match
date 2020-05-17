26eb09a7279a3761259b0e44df39e6a1185c5349
KAikawa10 <stbh1028@gmail.com>
2020-05-17 18:47:35 +0900

    レイアウトのblade内でvueを使用できるように修正

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -10,7 +10,7 @@
     <title>{{ config('app.name', 'Laravel') }}</title>
 
     <!-- Scripts -->
-    <script src="{{ asset('js/app.js') }}" defer></script>
+    <script src="{{ mix('js/app.js') }}" defer></script>
 
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
@@ -73,7 +73,7 @@
             </div>
         </nav>
 
-        <main class="py-4">
+        <main id="app" class="py-4">
             @yield('content')
         </main>
     </div>
