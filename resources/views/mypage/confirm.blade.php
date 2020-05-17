8f5601e84e511f6aa7f5b6a1d9f21976b85baf51
KAikawa10 <stbh1028@gmail.com>
2020-05-17 18:52:53 +0900

    プロフィール確認画面での表示の仕方を変更

--- resources/views/mypage/confirm.blade.php
+++ resources/views/mypage/confirm.blade.php
@@ -19,37 +19,37 @@
                     <tbody>
                         <tr>
                             <th scope="row">ユーザ名</th>
-                            <td>{{ $data->user_name }}</td>
+                            <td>{{ $data['user_name'] }}</td>
                         </tr>
                         <tr>
                             <th scope="row">年齢</th>
-                            <td>{{ $data->age }}</td>
+                            <td>{{ $data['age'] }}</td>
                         </tr>
                         <tr>
                             <th scope="row">居住地</th>
-                            <td>{{ $data->residence }}</td>
+                            <td>{{ $data['residence'] }}</td>
                         </tr>
                         <tr>
                             <th scope="row">職業</th>
-                            <td>{{ $data->job }}</td>
+                            <td>{{ $data['job'] }}</td>
                         </tr>
                         <tr>
                             <th scope="row">写真</th>
-                            <td>{{ $data->img_url }}</td>
+                            <td><img src="{{ $data['img_data'] }}"/ style="width:200px"></td>
                         </tr>
                         <tr>
                             <th scope="row">自己紹介文</th>
-                            <td>{{ $data->text }}</td>
+                            <td>{{ $data['text'] }}</td>
                         </tr>
                     </tbody>
                 </table>
 
-                <input type="hidden" id="user_name" name="user_name" value="{{ $data->user_name }}" class="form-control">
-                <input type="hidden" id="age" name="age" value="{{ $data->age }}" class="form-control">
-                <input type="hidden" id="residence" name="residence" value="{{ $data->residence }}" class="form-control">
-                <input type="hidden" id="job" name="job" value="{{ $data->job }}" class="form-control">
-                <input type="hidden" id="img_url" name="img_url" value="{{ $data->img_url }}" class="form-control">
-                <input type="hidden" id="text" name="text" value="{{ $data->text }}" class="form-control">
+                <input type="hidden" id="user_name" name="user_name" value="{{ $data['user_name'] }}" class="form-control">
+                <input type="hidden" id="age" name="age" value="{{ $data['age'] }}" class="form-control">
+                <input type="hidden" id="residence" name="residence" value="{{ $data['residence'] }}" class="form-control">
+                <input type="hidden" id="job" name="job" value="{{ $data['job'] }}" class="form-control">
+                <input type="hidden" id="img_data" name="img_data" value="{{ $data['img_data'] }}" class="form-control">
+                <input type="hidden" id="text" name="text" value="{{ $data['text'] }}" class="form-control">
 
                 <button class="btn btn-info my-4 btn-block" type="submit">ユーザ情報変更確認画面へ</button>
 
