7152985e1d09eef8072bfda8a9089cb1aa81c694
KAikawa10 <stbh1028@gmail.com>
2020-05-17 18:54:04 +0900

    コントローラ側でs3にデータを保存できるように実装

--- app/Http/Controllers/MypageController.php
+++ app/Http/Controllers/MypageController.php
@@ -3,9 +3,11 @@
 namespace App\Http\Controllers;
 
 use App\UserProfile;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Storage;
+use App\Http\Requests\UserProfileRequest;
 
 class MypageController extends Controller
 {
@@ -23,28 +25,85 @@ class MypageController extends Controller
 
     public function edit()
     {
-        return view('mypage.edit');
+        $user = Auth::user();
+
+        $user_profile = UserProfile::where('user_id', $user->id)->first();
+
+        return view('mypage.edit')
+                ->with('user_profile', $user_profile);
     }
 
-    public function confirm(Request $request)
+    public function confirm(UserProfileRequest $request)
     {
-       //Todo
+
+        $user = Auth::user();
+
+        $user_profile = UserProfile::where('user_id', $user->id)->first();
+
+        if(!empty($user_profile)){
+            $user_name = $user_profile->user_name === $request->user_name ? '' : $request->user_name;
+            $age = $user_profile->age == $request->age ? '' : $request->age;
+            $residence = $user_profile->residence === $request->residence ? '' : $request->residence;
+            $job = $user_profile->job === $request->job ? '' : $request->job;
+            
+            $text = $user_profile->text === $request->text ? '' : $request->text;
+        }else {
+            $user_name = $request->user_name;
+            $age = $request->age;
+            $residence = $request->residence;
+            $job = $request->job;
+            $text = $request->text;
+        }
+
+        $data = [
+            "user_name" => $user_name,
+            "age"       => $age,
+            "residence" => $residence,
+            "job"       => $job,
+            "img_data"  => $request->img_data,
+            "text"      => $text,
+        ];
+
+        //Todo
         //バリデーションをここで使う
-        return view('mypage.confirm')->with('data', $request);
+        return view('mypage.confirm')
+                ->with('data', $data);
     }
 
     public function store(Request $request)
     {
         $user = Auth::user();
-
         $user_profile = UserProfile::where('user_id', $user->id)->first();
 
         if(empty($user_profile)){
             $user_profile = new UserProfile;
             $user_profile->user_id = Auth::user()->id;
         }
+
+        $user_profile->user_name = is_null($request->user_name) ? $user_profile->user_name : $request->user_name;
+        $user_profile->age = is_null($request->age) ? $user_profile->age : $request->age;
+        $user_profile->residence = is_null($request->residence) ? $user_profile->residence : $request->residence;
+        $user_profile->job = is_null($request->job) ? $user_profile->job : $request->job;
+        $user_profile->text = is_null($request->text) ? $user_profile->text : $request->text;
+
+        if(!is_null($request->img_data)) {
+            $file_name = 'profileImg.png';
+            $file_directory = $user->id;
+            $file_path = $file_directory . "/" . $file_name;
+
+            if(Storage::disk('s3')->exists($file_path)) {
+                Storage::disk('s3')->delete($file_path);
+            }
+
+            $img_data = str_replace('data:image/png;base64,', '', $request->img_data);
+            $data = base64_decode($img_data);
         
-        $user_profile->fill($request->all());
+            file_put_contents("$file_name", $data);
+
+            $path = Storage::disk('s3')->putFile($file_directory, $file_name, "public");
+            $user_profile->img_url = Storage::disk('s3')->url($path);
+        }
+
         $user_profile->save();
 
         return redirect()->route('mypage.complete');
