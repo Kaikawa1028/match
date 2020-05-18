ec88029480b8f98af6c3454f46771dd47ddb514d
KAikawa10 <stbh1028@gmail.com>
2020-05-18 17:28:33 +0900

    コントローラをサービスを使うようにリファクタリング

--- app/Http/Controllers/MypageController.php
+++ app/Http/Controllers/MypageController.php
@@ -2,113 +2,74 @@
 
 namespace App\Http\Controllers;
 
-use App\UserProfile;
+use App\Model\UserProfile;
 use Carbon\Carbon;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
-use Illuminate\Support\Facades\Storage;
 use App\Http\Requests\UserProfileRequest;
+use App\Service\MypageService;
 
 class MypageController extends Controller
 {
+    private $mypage_service;
 
+    public function __construct(MypageService $mypage_service)
+    {
+        $this->mypage_service = $mypage_service;
+    }
 
+    /**
+     * マイページの表示
+     */
     public function index()
     {
         $user = Auth::user();
-        $user_profile = UserProfile::where('user_id', $user->id)->first();
+        $result = $this->mypage_service->showMypage($user->id);
 
         return view('mypage.index')
             ->with('user', $user)
-            ->with('user_profile', $user_profile);
+            ->with('result', $result);
     }
 
+    /**
+     * プロフィールの編集
+     */
     public function edit()
     {
         $user = Auth::user();
-
-        $user_profile = UserProfile::where('user_id', $user->id)->first();
+        $result = $this->mypage_service->showMypage($user->id);
 
         return view('mypage.edit')
-                ->with('user_profile', $user_profile);
+                ->with('result', $result);
     }
 
+    /**
+     * プロフィール編集の確認
+     */
     public function confirm(UserProfileRequest $request)
     {
 
         $user = Auth::user();
+        $result = $this->mypage_service->confirmProfile($user->id, $request->all());
 
-        $user_profile = UserProfile::where('user_id', $user->id)->first();
-
-        if(!empty($user_profile)){
-            $user_name = $user_profile->user_name === $request->user_name ? '' : $request->user_name;
-            $age = $user_profile->age == $request->age ? '' : $request->age;
-            $residence = $user_profile->residence === $request->residence ? '' : $request->residence;
-            $job = $user_profile->job === $request->job ? '' : $request->job;
-            
-            $text = $user_profile->text === $request->text ? '' : $request->text;
-        }else {
-            $user_name = $request->user_name;
-            $age = $request->age;
-            $residence = $request->residence;
-            $job = $request->job;
-            $text = $request->text;
-        }
-
-        $data = [
-            "user_name" => $user_name,
-            "age"       => $age,
-            "residence" => $residence,
-            "job"       => $job,
-            "img_data"  => $request->img_data,
-            "text"      => $text,
-        ];
-
-        //Todo
-        //バリデーションをここで使う
         return view('mypage.confirm')
-                ->with('data', $data);
+                ->with('result', $result);
     }
 
+    /**
+     * プロフィール情報の登録
+     */
     public function store(Request $request)
     {
         $user = Auth::user();
-        $user_profile = UserProfile::where('user_id', $user->id)->first();
-
-        if(empty($user_profile)){
-            $user_profile = new UserProfile;
-            $user_profile->user_id = Auth::user()->id;
-        }
-
-        $user_profile->user_name = is_null($request->user_name) ? $user_profile->user_name : $request->user_name;
-        $user_profile->age = is_null($request->age) ? $user_profile->age : $request->age;
-        $user_profile->residence = is_null($request->residence) ? $user_profile->residence : $request->residence;
-        $user_profile->job = is_null($request->job) ? $user_profile->job : $request->job;
-        $user_profile->text = is_null($request->text) ? $user_profile->text : $request->text;
-
-        if(!is_null($request->img_data)) {
-            $file_name = 'profileImg.png';
-            $file_directory = $user->id;
-            $file_path = $file_directory . "/" . $file_name;
-
-            if(Storage::disk('s3')->exists($file_path)) {
-                Storage::disk('s3')->delete($file_path);
-            }
-
-            $img_data = str_replace('data:image/png;base64,', '', $request->img_data);
-            $data = base64_decode($img_data);
-        
-            file_put_contents("$file_name", $data);
-
-            $path = Storage::disk('s3')->putFile($file_directory, $file_name, "public");
-            $user_profile->img_url = Storage::disk('s3')->url($path);
-        }
-
-        $user_profile->save();
+        $this->mypage_service->saveProfile($user->id, $request->all());
 
         return redirect()->route('mypage.complete');
     }
 
+    /**
+     * プロフィール変更の完了
+     */
     public function complete()
     {
         return view('mypage.complete');
