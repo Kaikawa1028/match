2e68c2f8453bc9c84daa7225e82961771c4c321a
KAikawa10 <stbh1028@gmail.com>
2020-05-23 16:02:41 +0900

    いいねをもらった、送った一覧表示のコントローラを作成

--- app/Http/Controllers/LikeController.php
+++ app/Http/Controllers/LikeController.php
@@ -6,15 +6,39 @@ use Illuminate\Http\Request;
 use App\User;
 use Carbon\Carbon;
 use App\Model\Like;
+use Illuminate\Support\Facades\Auth;
 
 class LikeController extends Controller
 {
+
+    public function receiveList()
+    {
+        $user  = Auth::user();
+        $likes = Like::with('receive_user.user_profile')->where('to_user_id', $user->id)->get();
+
+        return view('like.receive')
+                ->with('likes', $likes);
+    }
+
+    public function sendList()
+    {
+        $user  = Auth::user();
+        $likes = Like::with('send_user.user_profile')->where('from_user_id', $user->id)->get();
+
+        return view('like.send')
+                ->with('likes', $likes);
+    }
+
     public function send(Request $request, User $user)
     {
-        //$now = Carbon::now()->format('Y-m-d H:i:s');
+        $now = Carbon::now()->format('Y-m-d H:i:s');
 
         $request->user()->send_like_user()->detach($user);
-        $request->user()->send_like_user()->attach($user);
+        $request->user()->send_like_user()->attach(
+            $user,
+            ['created_at', $now],
+            ['updated_at', $now]
+        );
 
         return ['name' => $user->name];
     }
