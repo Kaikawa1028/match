78fef0dacd66a71899590f3ee3899bf2ddc3f53c
KAikawa10 <stbh1028@gmail.com>
2020-05-25 14:27:11 +0900

    roomモデルを追加

--- app/Model/Room.php
+++ app/Model/Room.php
@@ -9,4 +9,20 @@ class Room extends Model
     protected $fillable = [
         'man_user_id', 'woman_user_id'
     ]; 
+
+    public function man_user()
+    {
+        return $this->belongsTo('App\User', 'man_user_id');
+    }
+
+    public function woman_user()
+    {
+        return $this->belongsTo('App\User', 'woman_user_id');
+    }
+
+    public function messages()
+    {
+        return $this->hasMany('App\Model\Message');
+    }
+    
 }
