02564c36e68578e459f90a31a47182f694ffe595
KAikawa10 <stbh1028@gmail.com>
2020-05-26 13:16:39 +0900

    年齢、身長で範囲指定できるように変更

--- resources/views/user/index.blade.php
+++ resources/views/user/index.blade.php
@@ -9,26 +9,63 @@
                     <form action="{{ route('user') }}" method="get">
                         <div class="form-group">
                             <label for="age">年齢</label>
-                            <select id="age" name="age" class="form-control">
-                                <option value="">選択しない</option>
-                                <option value="25">25歳</option>
-                                <option value="26">26歳</option>
-                            </select>
+
+                            <div class="d-flex flex-row">
+                                <div class="w-25 mr-1">
+                                    <select id="age_higher" name="age_higher" class="form-control">
+                                        <option value="">選択しない</option>
+                                        <option value="25" @if(!is_null($request->age_higher) && $request->age_higher == 25) selected  @endif>25歳</option>
+                                        <option value="26" @if(!is_null($request->age_higher) && $request->age_higher == 26) selected  @endif>26歳</option>
+                                    </select>
+                                </div>
+                                <div class="align-self-end">以上</div>
+                                <div class="w-25 ml-4 mr-1">
+                                    <select id="age_lower" name="age_lower" class="form-control">
+                                        <option value="">選択しない</option>
+                                        <option value="25" @if(!is_null($request->age_lower) && $request->age_lower == 26) selected  @endif>25歳</option>
+                                        <option value="26" @if(!is_null($request->age_lower) && $request->age_lower == 26) selected  @endif>26歳</option>
+                                    </select>
+                                </div>
+                                <div class="align-self-end">以下</div>
+                            </div>
+                        </div>
+
+                        <div class="form-group">
+                            <label for="height">身長</label>
+
+                            <div class="d-flex flex-row">
+                                <div class="mr-1" style="width:30%;">
+                                    <select id="height_higher" name="height_higher" class="form-control">
+                                        <option value="">選択しない</option>
+                                        <option value="165" @if(!is_null($request->height_higher) && $request->height_higher == 165) selected  @endif>165cm</option>
+                                        <option value="170" @if(!is_null($request->height_higher) && $request->height_higher == 170) selected  @endif>170cm</option>
+                                    </select>
+                                </div>
+                                <div class="align-self-end">以上</div>
+                                <div class="ml-3 mr-1" style="width:30%;">
+                                    <select id="height_lower" name="height_lower" class="form-control">
+                                        <option value="">選択しない</option>
+                                        <option value="165" @if(!is_null($request->height_lower) && $request->height_lower == 165) selected  @endif>165cm</option>
+                                        <option value="170" @if(!is_null($request->height_lower) && $request->height_lower == 170) selected  @endif>170cm</option>
+                                    </select>
+                                </div>
+                                <div class="align-self-end">以下</div>
+                            </div>
                         </div>
 
                         <div class="form-group">
                             <label for="residence">居住地</label>
                             <select id="residence" name="residence" class="form-control">
                                 <option value="">選択しない</option>
-                                <option value="東京都">東京都</option>
-                                <option value="静岡県">静岡県</option>
+                                <option value="東京都" @if(!is_null($request->residence) && $request->residence == "東京都") selected  @endif>東京都</option>
+                                <option value="静岡県" @if(!is_null($request->residence) && $request->residence == "静岡県") selected  @endif>静岡県</option>
                             </select>
                         </div>
 
                         @can('paid-member')
                         <div class="form-group">
                             <label for="text">フリーワード</label>
-                            <input type="text" id="text" name="text" class="form-control" placeholder="犬・料理好き　など">
+                            <input type="text" id="text" name="text" class="form-control" placeholder="犬・料理好き　など" value="{{ $data['text'] ?? '' }}">
                         </div>
                         @endcan
 
@@ -52,7 +89,13 @@
             @endforeach
             </div>
             <div class="row justify-content">
-                {{ $result['users']->appends(['age' => Request::get('age'), 'residence' => Request::get('residence')])->links() }}
+                {{ $result['users']->appends([
+                                            'age_higher' => Request::get('age_higher'),
+                                            'age_lower' => Request::get('age_lower'),
+                                            'height_higher' => Request::get('height_higher'),
+                                            'height_lower' => Request::get('height_lower'),
+                                             'residence' => Request::get('residence')]
+                                             )->links() }}
             </div>
         </div>
     </div>
